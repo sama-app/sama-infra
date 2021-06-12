1f3316bf7ebdbf4bda86e13db2c15edd12cf0777
balysv <balys@valentukevicius.com>
2021-06-12 16:48:17 +0200

    Use spring profiles for deployment

--- sama-service/scripts/deploy.sh
+++ sama-service/scripts/deploy.sh
@@ -8,7 +8,7 @@ eval "$(aws ecr get-login --region eu-central-1 --no-include-email)"
 docker volume create sama-service-logs
 docker run -d \
       --name sama-service \
-      -e X_JAVA_OPTS="-Dspring.datasource.host=sama-dev.cp9s2aovpufd.eu-central-1.rds.amazonaws.com -Dspring.datasource.port=5432 -Dspring.datasource.username=sama -Dspring.datasource.password=kvta26gvQTzgbJKL2XZP" \
+      -e X_JAVA_OPTS="-Dspring.profiles.active=dev" \
       -p 3000:3000 \
       -v sama-service-logs:/var/log/sama/sama-service \
       216862985054.dkr.ecr.eu-central-1.amazonaws.com/sama-service:latest
